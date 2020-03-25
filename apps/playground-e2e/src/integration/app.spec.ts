c03f673d064fb3c43fa3e01ce288479e103a5691
Netanel Basal <netanel7799@gmail.com>
2020-03-25 06:36:13 +0200

    test: 💍 add tests

--- apps/playground-e2e/src/integration/app.spec.ts
+++ apps/playground-e2e/src/integration/app.spec.ts
@@ -3,42 +3,62 @@ import {
   getConfirmationStay,
   getConfirmModal,
   getInput,
-  navigateHome
+  navigateHome,
+  getSaveBtn,
+  navigateSettings
 } from '../support/app.po';
 
+function fill(text: string) {
+  getInput().clear({ force: true });
+  getInput().type(text);
+  cy.tick(300);
+}
+
+const makeDirty = () => fill('Dirty');
+const revertToInitial = () => fill('Initial Value');
+
 describe('playground', () => {
-  beforeEach(() => cy.visit('/settings'));
+  before(() => cy.visit('/settings'));
 
-  it('should confirm navigation', () => {
-    getInput().type('New input');
-    cy.wait(300);
-    navigateHome();
-    getConfirmModal().should('be.visible');
-  });
-
-  it('should stay on confirmation stay', () => {
-    getInput().type('New');
-    cy.wait(300);
-    navigateHome();
-    getConfirmationStay().click();
-    cy.url().should('contain', 'settings');
-  });
-
-  it('should leave on confirmation leave', () => {
-    getInput().type('New');
-    cy.wait(300);
-    navigateHome();
-    getConfirmationLeave().click();
-    cy.url().should('not.contain', 'settings');
-  });
-
-  it('should prompt browser confirmation', () => {
-    getInput().type('New');
-    cy.wait(500);
-    Cypress.on('window:before:unload', (e: BeforeUnloadEvent) => {
-      console.log(`ret=${e.returnValue}`);
-      expect(e.returnValue).equal(false);
+  context('Save Button', () => {
+    it('should show toggle Save button', () => {
+      cy.clock();
+      makeDirty();
+      getSaveBtn().should('be.visible');
+      revertToInitial();
+      getSaveBtn().should('not.be.visible');
     });
-    cy.reload();
+  });
+
+  context('Confirmation Dialog', () => {
+    it('should show confirm navigation', () => {
+      cy.clock();
+      makeDirty();
+      navigateHome();
+      getConfirmModal().should('be.visible');
+    });
+
+    it('should stay on confirmation stay', () => {
+      getConfirmationStay().click();
+      cy.url().should('contain', 'settings');
+    });
+
+    it('should leave on confirmation leave', () => {
+      navigateHome();
+      getConfirmationLeave().click();
+      cy.url().should('not.contain', 'settings');
+    });
+
+    it('should not show the modal when it is not dirty again', () => {
+      cy.clock();
+      navigateSettings();
+      makeDirty();
+      navigateHome();
+      getConfirmModal().should('be.visible');
+      getConfirmationStay().click();
+      revertToInitial();
+      navigateHome();
+      cy.url().should('not.contain', 'settings');
+    });
   });
 });
