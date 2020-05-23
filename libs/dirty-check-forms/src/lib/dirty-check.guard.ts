d79b0ef13892b322ed6dd67548d2a5a4501fd696
Dan Roujinsky <danr@siemplify.co>
2020-05-23 11:10:25 +0300

    refactor(dirty-guard): use defer for function

--- libs/dirty-check-forms/src/lib/dirty-check.guard.ts
+++ libs/dirty-check-forms/src/lib/dirty-check.guard.ts
@@ -1,9 +1,9 @@
 import {
   ActivatedRouteSnapshot,
   CanDeactivate,
-  RouterStateSnapshot
+  RouterStateSnapshot,
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { defer, Observable, of } from 'rxjs';
 import { switchMap, take } from 'rxjs/operators';
 import { DirtyComponent } from './dirty-component';
 import { isFunction, toObservable } from './utils/coercion';
@@ -14,12 +14,17 @@ export abstract class DirtyCheckGuard implements CanDeactivate<DirtyComponent> {
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot
   ): Observable<boolean> {
-    const dirty$ = isFunction(component.isDirty$)
-      ? toObservable(component.isDirty$())
-      : toObservable(component.isDirty$);
+    let dirty$;
+    const componentDirty = component.isDirty$;
+
+    if (isFunction(componentDirty)) {
+      dirty$ = defer(() => toObservable(componentDirty()));
+    } else {
+      dirty$ = toObservable(componentDirty);
+    }
 
     return dirty$.pipe(
-      switchMap(isDirty => {
+      switchMap((isDirty) => {
         if (isDirty === false) {
           return of(true);
         }
